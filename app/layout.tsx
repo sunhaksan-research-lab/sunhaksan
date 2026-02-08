f287bef9ff2d348a1e9dd2c96a4bf9f8741f3eba
net777oz <net777oz@gmail.com>
2026-02-08 19:46:15 +0900

    Remove Navigation and SessionProvider from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "@/components/navigation";
-import { SessionProvider } from "@/components/session-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,10 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>
-          <Navigation />
-          <main>{children}</main>
-        </SessionProvider>
+        <main>{children}</main>
       </body>
     </html>
   );
